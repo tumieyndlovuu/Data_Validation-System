34f9c9cdf32f82c39931b075303806301dec9e4c
Tumisang Ndhlovu <tumisangndhlovu779@gmail.com>
2026-09-22 11:08:45 +0200

    Add files via upload

---
+++ Advanced.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package advanced;
+
+/**
+ *
+ * @author tumis
+ */
+public class Advanced {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
